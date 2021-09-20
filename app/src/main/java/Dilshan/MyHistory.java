af670e5ebdc5852ec5ea6a61d2cc3c6af9b2539b
dilshanhiruna <dilshan.hirunaa@gmail.com>
2021-09-20 23:46:31 +0530

    Tab Buttons

---
+++ app/src/main/java/Dilshan/MyHistory.java
@@ -0,0 +1,16 @@
+package Dilshan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uee.singercare.R;
+
+public class MyHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_history);
+    }
+}
