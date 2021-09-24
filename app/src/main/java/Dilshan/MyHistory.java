92c2a96b8c782e38224661c5913dabef637ca0a2
dilshanhiruna <dilshan.hirunaa@gmail.com>
2021-09-24 19:14:30 +0530

    Created Fragments - OngoingFragment/ HistoryFragment

--- app/src/main/java/Dilshan/MyHistory.java
+++ app/src/main/java/Dilshan/MyHistory.java
@@ -1,16 +1,51 @@
 package Dilshan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.uee.singercare.R;
 
 public class MyHistory extends AppCompatActivity {
 
+    Button ongoingBtn, historyBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_history);
+        ongoingBtn = findViewById(R.id.ongoing_history_btn);
+        historyBtn = findViewById(R.id.history_history_btn);
+
+        ongoingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new OngoingFragment());
+            }
+        });
+        historyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new HistoryFragment());
+            }
+        });
+
+
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+
     }
 }
