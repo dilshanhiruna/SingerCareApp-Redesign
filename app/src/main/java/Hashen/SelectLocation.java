bb346377cb2daca0b8792a704195623a31f49ad5
99hash63 <thkottegoda@gmail.com>
2021-10-09 23:33:11 +0530

    Created send inquiry interface

---
+++ app/src/main/java/Hashen/SelectLocation.java
@@ -0,0 +1,16 @@
+package Hashen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.uee.singercare.R;
+
+
+public class SelectLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_location);
+    }
+}
